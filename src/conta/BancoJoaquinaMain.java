23dfc6e005ca3f244db8a1df635610644ca4a059
Ericsson Tribess <ericktribess@gmail.com>
2023-02-15 19:49:47 -0300

    Saque Banco Joaquina

--- src/conta/BancoJoaquinaMain.java
+++ src/conta/BancoJoaquinaMain.java
@@ -30,6 +30,29 @@ public class BancoJoaquinaMain {
 				break;
 			case 3:
 				System.out.println("Saque selecionado");
+				// Solicitar o número da conta
+				System.out.print("Digite o número da conta: ");
+				int numeroConta = scanner.nextInt();
+				// Solicitar o valor do saque
+				System.out.print("Digite o valor do saque: ");
+
+				double valorSaque = scanner.nextDouble();
+				// Exibir as informações inseridas pelo usuário
+				System.out.println("Número da conta: " + numeroConta);
+				System.out.println("Valor do saque: " + valorSaque);
+				for (conta.Conta conta : contas) {
+					if (conta.getNumero() == numeroConta) {
+						if (conta.getSaldo() >= valorSaque) {
+							double novoSaldo = conta.getSaldo() - valorSaque;
+							conta.setSaldo(novoSaldo);
+							boolean saqueRealizado = true;
+							if (saqueRealizado) {
+								System.out.println("Saque realizado com sucesso!");
+							}
+
+							else {
+								System.out.println("Não foi possível realizar o saque.");
+							}
 				break;
 			case 4:
 				System.out.println("Transferência selecionada");
